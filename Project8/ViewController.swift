0e019e432ccb55a35525fd42180b4ec8b940923c
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-14 10:15:45 +0900

    first commit

---
+++ Project8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project8
+//
+//  Created by Yoo Hwa Park on 2020/04/14.
+//  Copyright © 2020 Yoo Hwa Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
